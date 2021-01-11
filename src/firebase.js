3b664f9399ed7e8a419414ea2b356fe4ca9c565a
Cengiz <cengizkemaloglu@gmail.com>
2021-01-11 23:00:37 +0000

    env değişikliği

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,8 @@ import firestore from 'firebase/firestore'
 //const settings = {timestampsInSnapshots: true};
 
 const config = {
-    apiKey: "AIzaSyD2U-4w-FU7U1Jh8ZQkid-LUn4PWmiASKM",
+    //apiKey: "AIzaSyD2U-4w-FU7U1Jh8ZQkid-LUn4PWmiASKM",
+    apiKey: process.env.REACT_APP_API_KEY,
     authDomain: "minienvanter.firebaseapp.com",
     databaseURL: "https://minienvanter.firebaseio.com",
     projectId: "minienvanter",
@@ -13,6 +14,12 @@ const config = {
     messagingSenderId: "147745854938",
     appId: "1:147745854938:web:7cd95dbb6e0545d4a51ce3"
 };
+console.log("GELDİİİİ");
+console.log("API", process.env.REACT_APP_API_KEY)
+
+//console.log("APİ : " + process.env.API_KEY);
+//console.log("APİ : " + `${process.env.API_KEY}`);
+
 firebase.initializeApp(config);
 
 firebase.firestore();
